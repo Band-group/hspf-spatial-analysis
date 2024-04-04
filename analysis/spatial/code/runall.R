a4fcbba8cc144ea0ab84432c2022150a8011c01e
Andre Python <python.andre@gmail.com>
2024-04-04 11:32:34 +0800

    add timer to compute time to run code in runall

--- analysis/spatial/code/runall.R
+++ analysis/spatial/code/runall.R
@@ -1,6 +1,15 @@
 #code to reproduce the work
+#install packages
+list.of.packages <- c("tictoc")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
+#start timer to compute time to run session
+tic()
 
 source("code/priors.R",verbose=FALSE)
 source("code/functions.R",verbose=FALSE)
 source("code/HbS_model_fit.R",verbose=FALSE)
 source("code/HbS_model_diagnostics.R",verbose=FALSE)
+
+toc()#provide time used to run the code
