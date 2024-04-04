372a0b2a63bd1900fcb8e7b4837cf4f053aecfb6
Andre Python <python.andre@gmail.com>
2024-04-04 11:29:18 +0800

    Create a new R code to reproduce the work (source all code)

---
+++ analysis/spatial/code/runall.R
@@ -0,0 +1,6 @@
+#code to reproduce the work
+
+source("code/priors.R",verbose=FALSE)
+source("code/functions.R",verbose=FALSE)
+source("code/HbS_model_fit.R",verbose=FALSE)
+source("code/HbS_model_diagnostics.R",verbose=FALSE)
