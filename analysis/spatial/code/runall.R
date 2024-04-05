84948dab6500f8b1f9e71cce6fe16faf7e544844
Andre Python <python.andre@gmail.com>
2024-04-05 22:25:10 +0800

    Add additional user parameters that can be chosen for Pf model and add also other shapefiles that are required.

--- analysis/spatial/code/runall.R
+++ analysis/spatial/code/runall.R
@@ -7,7 +7,27 @@ lapply(list.of.packages, library, character.only = TRUE)
 
 #parameters
 minpf <- 5 #minimum number of observations to filter pf data
-nn <- 500 #nb.HbS samples per pixel for HbS maps
+nn <- 500 #nb. estimated HbS value samples per pixel for HbS maps
+nnpf <- 500 #nb. estimated Pf value samples per pixel for Pf maps
+#pf range and sigma for rob tests
+myrangerob <- c(0.5,1.5,2.5,4,6)
+mysigmarob <- c(0.1,1,5,10,15)
+Prangerob <- 0.1#if 0 means that range0 is fixed
+Psigmarob <- 0.1#if 0 means that sigma0 is fixed
+r0_manuscript <- 2
+sigma0_manuscript <- 10
+################################################################################
+#Computer resources parameters##################################################
+################################################################################
+#set the number of free logical cores [0;(nbcores-1)]
+freecores <- 2 
+#(2 is recommended; 0: all resources will be used to run the code)
+nbcores <- parallel::detectCores() - freecores
+maxRcores <- 124#124 is the max possible nb cores for R
+#automatic (no user needed below)
+#different code optimization for different machine types
+if(nbcores >= 64){highmem <- TRUE} else {highmem <- FALSE }
+
 #start timer to compute time to run session
 tic()
 
@@ -18,9 +38,12 @@ myarea <- load.continent.shapes.terra(
   "Africa"
 )
 africa = load.entry.from.Rdata( "geodata/naturalearthdata.Rdata", "africa" )
+world_sf = load.entry.from.Rdata( "geodata/naturalearthdata.Rdata", "world_sf" )
+ocean_sf = load.entry.from.Rdata( "geodata/naturalearthdata.Rdata", "ocean_sf" )
+continents_sf = load.entry.from.Rdata( "geodata/naturalearthdata.Rdata", "continents_sf" )
 rivaf_sf = load.entry.from.Rdata( "geodata/naturalearthdata.Rdata", "rivaf_sf" )
 lakaf_sf = load.entry.from.Rdata( "geodata/naturalearthdata.Rdata", "lakaf_sf" )
-africa_sf = sf::st_as_sf(africa) 
+africa_sf = load.entry.from.Rdata( "geodata/naturalearthdata.Rdata", "africa_sf" )
 
 
 #HbS################################################
@@ -32,6 +55,6 @@ source("code/HbS_model_diagnostics.R",verbose=FALSE)
 #HbS################################################
 
 #Pf#################################################
-source("Pf_Datacleaning.R",verbose=FALSE)
-
+source("code/Pf_datacleaning.R",verbose=FALSE)
+source("code/Pf_model_fit.R",verbose=FALSE)#aspatial Pf models
 toc()#provide time used to run the code
