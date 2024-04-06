e88b011323e25cb9b9b02a4634e23496db6f70f4
Andre Python <python.andre@gmail.com>
2024-04-06 09:25:20 +0800

    add a few minor changes in runall and include sourcing Pf spatial models

--- analysis/spatial/code/runall.R
+++ analysis/spatial/code/runall.R
@@ -15,6 +15,8 @@ source("code/functions.R",verbose=FALSE)
 minpf <- 5 #minimum number of observations to filter pf data
 nn <- 500 #nb. estimated HbS value samples per pixel for HbS maps
 nnpf <- 500 #nb. estimated Pf value samples per pixel for Pf maps
+nnpf <- 500 #nb. estimated Pf value samples per pixel for Pf maps
+
 #pf range and sigma for rob tests
 myrangerob <- c(0.5,1.5,2.5,4,6)
 mysigmarob <- c(0.1,1,5,10,15)
@@ -34,6 +36,9 @@ maxRcores <- 124#124 is the max possible nb cores for R
 #different code optimization for different machine types
 if(nbcores >= 64){highmem <- TRUE} else {highmem <- FALSE }
 
+#If not defined otherwise, use this theme for all plots
+ggplot2::theme_set(ggthemes::theme_few(base_size = 14, base_family = "serif"))
+
 #start timer to compute time to run session
 tic()
 
@@ -51,7 +56,6 @@ rivaf_sf = load.entry.from.Rdata( "geodata/naturalearthdata.Rdata", "rivaf_sf" )
 lakaf_sf = load.entry.from.Rdata( "geodata/naturalearthdata.Rdata", "lakaf_sf" )
 africa_sf = load.entry.from.Rdata( "geodata/naturalearthdata.Rdata", "africa_sf" )
 
-
 #HbS################################################
 source("code/HbS_model_fit.R",verbose=FALSE)
 source("code/HbS_model_diagnostics.R",verbose=FALSE)
@@ -62,7 +66,9 @@ source("code/HbS_model_diagnostics.R",verbose=FALSE)
 source("code/Pf_datacleaning.R",verbose=FALSE)
 source("code/Pf_model_fit.R",verbose=FALSE)#aspatial Pf models
 source("code/Pf_plots.R",verbose=FALSE)#pf plots
+source("code/Pf_model_spatial.R",verbose=FALSE)#spatial Pf models
 
+#About xxmn with AMD 3975WX 32 cores################
 #End Pf#############################################
 
 toc()#provide time used to run the code
