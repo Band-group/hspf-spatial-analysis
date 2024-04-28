efe39f1623539573778c33b7fb3a5f4b9dd0ca9b
Andre Python <python.andre@gmail.com>
2024-04-28 14:16:49 +0800

    change option outcome (no DRC split)

--- analysis/spatial/code/Runtest.R
+++ analysis/spatial/code/Runtest.R
@@ -43,7 +43,7 @@ if(nbcores >= 64){highmem <- TRUE} else {highmem <- FALSE }
 #West African countries aggregation option 
 #Aggregate Senegal, Gambia, and Guinea since data sample too small
 senegambea <- TRUE#if TRUE, aggregate Senegal, Gambia, and Guinea-Bissau
-DRCsplit <- TRUE#if TRUE, keep only Pf data in south of DRC (below -2.5 lat) (for test)
+DRCsplit <- FALSE#if TRUE, keep only Pf data in south of DRC (below -2.5 lat) (for test)
 cposel <- FALSE#if TRUE, use cpo to select best model; FALSE: use waic
 
 ################################################################################
