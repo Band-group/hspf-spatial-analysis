285d67c4a398b8173404b1602f2bd30668a68b0a
Andre Python <python.andre@gmail.com>
2024-05-27 14:50:30 +0800

    lots of change to make prepare for diagnostic plots and fig1.plot

--- analysis/spatial/code/HbS_model_diagnostics.R
+++ analysis/spatial/code/HbS_model_diagnostics.R
@@ -1,7 +1,10 @@
 library( RSQLite )
 library( ggplot2 )
 library( scico )
-library( scales)
+library( scales )
+library( fasterize )
+library( DBI )
+library( dplyr)
 source( 'code/Functions.R' )
 source( 'code/Priors.R' )
 
@@ -17,14 +20,14 @@ mkdir_recursive(
 )
 
 
-echo( "++ Loading population mask from %s...", "geodata/pop100m.tif" )
+echo( "++ Loading population mask from %s...", "gpw-v4-population-density_2000.tif" )
 #load data for prediction 
 #load pop raster for popmasking
-popmask <- raster("geodata/pop100m.tif")
+popmask <- raster("geodata/gpw4_2000_lowres.tif")
 # here we can define various thresholds for the mask
 # threshold unit in inhabitants per km2
-popmask[popmask <= 0.05] <- NA #orignal threshold: 0.05
-popmask[popmask > 0.05] <- 1 #orignal threshold: 0.05
+popmask[popmask <= 0.1] <- NA #orignal threshold: 0.05
+popmask[popmask > 0.1] <- 1 #orignal threshold: 0.05
 
 ############################################################xyt####################
 #Response and covariate extraction and data preparation for R-INLA model
@@ -35,7 +38,12 @@ popmask[popmask > 0.05] <- 1 #orignal threshold: 0.05
 echo( "++ Loading cleaned HbS data from %s...", "input/cleanHbSdata.csv" )
 HbSdata <- read.csv("input/cleanHbSdata.csv")
 pt = dfToSpatialPts( HbSdata )
-mycheck = check.excluded( pt, africa )
+if(worldsel==FALSE){
+  mycheck = check.excluded( pt, africa )
+} else {
+  world <- as(world_sf,"Spatial")
+  mycheck = check.excluded( pt, world )
+}
 echo( "!! insample_diagnosis.R: number of observations excluded: %d", nrow(mycheck$excluded@data ))
 
 # keep only observations in study area
@@ -45,8 +53,9 @@ xyt <- pt[extpoly, ]
 
 # Pf data
 echo( "++ Loading pf data from %s, by_site table...", "input/hbs-pf.sqlite" )
-db = dbConnect( dbDriver( "SQLite" ), "input/hbs-pf.sqlite" )
-pf = dbGetQuery( db, "SELECT * FROM by_site" )
+db = DBI::dbConnect(DBI::dbDriver( "SQLite" ), "input/hbs-pf.sqlite" )
+pf = DBI::dbGetQuery( db, "SELECT * FROM by_site" )
+DBI::dbDisconnect(db)
 pf = (
 	pf
 	%>% mutate(
@@ -62,19 +71,11 @@ proj4string(pf) = proj4string(africa)
 pf = pf[extpoly,]
 echo( "++ ...of which %d are in Africa.", nrow( pf@data ))
 
-# Map prediction locations
-# echo( "++ Generating map prediction locations from geodata::elevation_global() data..." )
-# pred_locs = get_prediction_locations(
-#   geodata::elevation_global( res=10, path = "geodata/" ),
-#   myarea,
-#   masked_features = list( lakes = lakaf_sf )
-# )
-
 #load Piel's map, needed for visualisatio
 echo( "++ Loading Piel et al map from %s...", "geodata/2013_Sickle_Haemoglobin_HbS_Allele_Freq_Global_5k_Decompressed.tif" )
 HbSPiel <- raster("geodata/2013_Sickle_Haemoglobin_HbS_Allele_Freq_Global_5k_Decompressed.tif")
 
-echo( "++ Genrating colour scheme..." )
+echo( "++ Generating colour scheme..." )
 colbreak <- c(0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.1, 0.12, 0.14, 0.16, 0.18, 0.20, 0.22, 1)
 color.scheme = tibble(
   breaks = c( 0.00, colbreak ),
@@ -85,74 +86,97 @@ color.scheme = tibble(
 
 echo( "++ Ok, making diagnostic plots in %s...", "output/HbSsensitivity/diagnostics" )
 dir.create( "output/HbSsensitivity/diagnostics")
-in.sample.summary = tibble()
 HbS.priors = priors()
-
 #Run in foreach loop to save time
+# in.sample.summary = tibble()
+# for( i in 1:nrow( HbS.priors )) {
+#   prior = HbS.priors[i,]
+#   message( sprintf( "++ Creating diagnostic plot for prior %s...", prior$name ))
+#   modelfit = readRDS( sprintf( "output/HbSsensitivity/fits/%s-modelfit.rds", prior$name ))
+#   predictions = readRDS( sprintf( "output/HbSsensitivity/fits/%s-predictions.rds", prior$name ))
+#   posterior.samples = readRDS( sprintf( "output/HbSsensitivity/fits/%s-samples.rds", prior$name ))
+  
+#   if(worldsel==FALSE){
+#     spatialdomain <- africa_sf
+#   } else {
+#     spatialdomain <- world_sf
+#   }
+#   plots = generate_diagnostic_plot(
+# 	  xyt,
+#       modelfit,
+#       predictions,
+#       HbSPiel,
+#       features = list(
+#         spatialdomain = spatialdomain,
+#         rivers = rivaf_sf,
+#         lakes = lakaf_sf
+#       ),
+#       color.scheme = color.scheme,
+#       prednames = c("mean", "sd", "iqr" ), # Choose three from mean, q25, q50, q75, sd, iqr
+# 	  popmask = popmask,
+# 	  saveraster = FALSE,
+# 	  saverastername = 'HbS'
+#   )
 
-for( i in 1:nrow( HbS.priors )) {
-  prior = HbS.priors[i,]
-  message( sprintf( "++ Creating diagnostic plot for prior %s...", prior$name ))
-  modelfit = readRDS( sprintf( "output/HbSsensitivity/fits/%s-modelfit.rds", prior$name ))
-  predictions = readRDS( sprintf( "output/HbSsensitivity/fits/%s-predictions.rds", prior$name ))
-  posterior.samples = readRDS( sprintf( "output/HbSsensitivity/fits/%s-samples.rds", prior$name ))
+#   pf_location_predictions = predict_inla_binomial_model(
+#     posterior.samples,
+#     modelfit$mesh,
+#     pf,
+#     nn
+#   )
 
-  plots = generate_diagnostic_plot(
-	  xyt,
-      modelfit,
-      predictions,
-      HbSPiel,
-      features = list(
-        africa = africa_sf,
-        rivers = rivaf_sf,
-        lakes = lakaf_sf
-      ),
-      color.scheme = color.scheme,
-      prednames = c("mean", "sd", "iqr" ), # Choose three from mean, q25, q50, q75, sd, iqr
-	  popmask = popmask,
-	  saveraster = FALSE,
-	  saverastername = 'HbS'
-  )
+#   pf@data$HbS_mean = pf_location_predictions$mean
+#   pf@data$S_mean = 2*pf@data$HbS_mean*(1-pf@data$HbS_mean) + pf@data$HbS_mean*pf@data$HbS_mean ;
 
-  pf_location_predictions = predict_inla_binomial_model(
-    posterior.samples,
-    modelfit$mesh,
-    pf,
-    nn
-  )
-
-  pf@data$HbS_mean = pf_location_predictions$mean
-  pf@data$S_mean = 2*pf@data$HbS_mean*(1-pf@data$HbS_mean) + pf@data$HbS_mean*pf@data$HbS_mean ;
-
-  plots$pf = (
-    ggplot( data = pf@data, aes( x = HbS_mean, y = Pfsa1_freq, colour = source ) )
-    + geom_segment( aes( x = S_mean, xend = S_mean, y = Pfsa1_lower, yend = Pfsa1_upper ))
-    + geom_point( aes( size = Pfsa1_N ))
-    + scale_size_binned()
-    + geom_smooth( method = 'glm', method.args = list( family="binomial") )
-    + facet_wrap( ~country, scales = "free" )
-    + xlab( "HbS frequency (mean)")
-    + ylab( "Pfsa1+ frequency and 95% CI")
-    + theme_minimal()
-  )
-  stub = sprintf( "output/HbSsensitivity/diagnostics/%s", prior$name )
-  ggsave( plots$unmasked, file = sprintf( "%s-diagnostics.pdf", stub ), width = 14.5, height = 10 )
-  ggsave( plots$masked, file = sprintf( "%s-masked-diagnostics.pdf", stub ), width = 14.5, height = 10 )
-  ggsave( plots$pf, file = sprintf( "%s-pf.pdf", stub ), width = 14.5, height = 10 )
-  plots$in.sample.summary$name = prior$name
-  plots$in.sample.summary$priorid <- ifelse(plots$in.sample.summary$type == 'piel', NA, i)
-  #extract cpo and waic values (out-of-sample and in-sample metric) for our model (NA if taken from piel)
-  plots$in.sample.summary$cpo <- ifelse(plots$in.sample.summary$type == 'piel', NA, -1*mean(log(modelfit$fit$cpo$cpo+0.1), na.rm = TRUE))
-  plots$in.sample.summary$waic <- ifelse(plots$in.sample.summary$type == 'piel', NA, modelfit$fit$waic$waic)
-  in.sample.summary <- bind_rows( in.sample.summary, plots$in.sample.summary )
+#   plots$pf = (
+#     ggplot( data = pf@data, aes( x = HbS_mean, y = Pfsa1_freq, colour = source ) )
+#     + geom_segment( aes( x = S_mean, xend = S_mean, y = Pfsa1_lower, yend = Pfsa1_upper ))
+#     + geom_point( aes( size = Pfsa1_N ))
+#     + scale_size_binned()
+#     + geom_smooth( method = 'glm', method.args = list( family="binomial") )
+#     + facet_wrap( ~country, scales = "free" )
+#     + xlab( "HbS frequency (mean)")
+#     + ylab( "Pfsa1+ frequency and 95% CI")
+#     + theme_minimal()
+#   )
+#   stub = sprintf( "output/HbSsensitivity/diagnostics/%s", prior$name )
+#   ggsave( plots$unmasked, file = sprintf( "%s-diagnostics.pdf", stub ), width = 14.5, height = 10 )
+#   ggsave( plots$masked, file = sprintf( "%s-masked-diagnostics.pdf", stub ), width = 14.5, height = 10 )
+#   ggsave( plots$pf, file = sprintf( "%s-pf.pdf", stub ), width = 14.5, height = 10 )
+#   plots$in.sample.summary$name = prior$name
+#   plots$in.sample.summary$priorid <- ifelse(plots$in.sample.summary$type == 'piel', NA, i)
+#   #extract cpo and waic values (out-of-sample and in-sample metric) for our model (NA if taken from piel)
+#   plots$in.sample.summary$cpo <- ifelse(plots$in.sample.summary$type == 'piel', NA, -1*mean(log(modelfit$fit$cpo$cpo+0.1), na.rm = TRUE))
+#   plots$in.sample.summary$waic <- ifelse(plots$in.sample.summary$type == 'piel', NA, modelfit$fit$waic$waic)
+#  in.sample.summary <- bind_rows( in.sample.summary, plots$in.sample.summary )
   #ONLY FOR BEST MODEL###########################################################
   #at the end of the procedure makes HbS map for figure 1 based on the best performing model
-  if(i == nrow( HbS.priors ))
-  {
-    if(cposel == FALSE){
-      in.sample.summary <- in.sample.summary %>% arrange (waic) 
+
+# library(foreach)
+# library(doParallel)
+# nbcores <- pmin(maxRcores, availableCores(omit = freecores),nrow(HbS.priors)*2)
+# cl <- makeCluster(nbcores)
+# registerDoParallel(cl)
+# list.of.packages <- c("terra", "raster","sf","stats", "rasterVis","cowplot", "viridis", "geodata", "rnaturalearth", "malariaAtlas", "readxl","ggplot2",
+#                       "RColorBrewer","ggthemes", "ggmap", "rgdal", "rgeos","maptools", "tmap","gtools","purrr","ggdist","inlabru","mapproj","scico","scales",
+#                       "parallelly","parallel","foreach","dplyr","rbenchmark","gridExtra","tibble","tidyr","elevatr","INLAspacetime","fmesher","fields","readr", "Metrics")
+#all.summary <- foreach(i = 1:nrow(HbS.priors), .packages = list.of.packages,.combine = rbind) %dopar% {
+#  diagnostic_plot_priors(i)
+#}
+# stopCluster(cl)
+# registerDoSEQ() 
+# gc()
+
+all.summary <- foreach(i = 1:nrow(HbS.priors), .packages = list.of.packages,.combine='rbind') %do% {
+  diagnostic_plot_priors(i)
+}
+message("++ Great success! Loop of diagnostic plots computation completed." )
+  # if(i == nrow( HbS.priors ))
+  # {
+   if(cposel == FALSE){
+      in.sample.summary <- all.summary %>% arrange (waic) 
     } else {
-      in.sample.summary <- in.sample.summary %>% arrange (cpo)
+      in.sample.summary <- all.summary %>% arrange (cpo)
     }
   readr::write_csv(
       (
@@ -173,8 +197,8 @@ for( i in 1:nrow( HbS.priors )) {
         %>% filter( type == 'ours' | name == 'fixed-r0=2.5-sigma0=0.1' )
       )
     )
-  #identify where cpo is highest
-    best_model <- in.sample.summary[1,]$name#first row is best model cause (increasing by CPO)
+  #identify where cpo or waic is lowest
+    best_model <- in.sample.summary[1,]$name#first row is best model cause (increasing by CPO or WAIC)
     best_id <- in.sample.summary[1,]$priorid
     prior = HbS.priors[best_id,]
     message( sprintf( "++ Creating figure 1 plot based on model with prior %s...", prior$name ))
@@ -184,18 +208,30 @@ for( i in 1:nrow( HbS.priors )) {
     
     #save HbS raster maps based on best model
     myraster <- generate_raster_maps( predictions,saveraster=TRUE,saverastername = 'HbS',savepath='output/HbSraster/')
-    
     #make figure 1 (top panels: a,b, and c)
-    fig1.plot(datasource=best_model,pfpt=pf,xyt=xyt,hbsraster=plots$meanmask,border=africa_sf,river=rivaf_sf,lake=lakaf_sf,
-                          scicopalette = 'turku',savepath = 'output/fig1')
+    if(worldsel==TRUE){
+      myheight <- 8;mywidth <- 16;myproj <- 'mollweide';
+    #simplified worldmap, not sure how to use it properly for background map
+    #worldmap = ggplot2::map_data("world") %>% dplyr::filter(! (region == 'Alaska' | lat < -60 | long < -125 ))
+    smallworld = sf::st_as_sfc(sf::st_bbox(c(xmin = -150, ymin = -60, xmax = 170, ymax = 90),crs = 4326))
+    spatialdomain <- sf::st_intersection(world_sf, smallworld)
+    #spatialdomain <- world_sf
+    } else {
+      myheight <- 8;mywidth <- 8;myproj <- NA;spatialdomain <- africa_sf
+    }
+    trunchbsraster <- myraster$mean
+    #OPTION####truncate values if above observed HbS##############################################################
+    trunchbsraster[trunchbsraster > max(xyt$S/xyt$N,na.rm=TRUE)] <- max(xyt$S/xyt$N,na.rm=TRUE)
+    #END OPTION####truncate values if above observed HbS##########################################################
+    fig1.plot(datasource=best_model,pfpt=pf,xyt=xyt,hbsraster=trunchbsraster,border=spatialdomain,river=rivaf_sf,lake=lakaf_sf,
+                          scicopalette = 'berlin',savepath = 'output/fig1',myheight=myheight,mywidth=mywidth,myproj=myproj)
+                          #originally: hbsraster=plots$masked
     #save best model name
     readr::write_csv( ( as.data.frame( best_model ) ),
       file = "output/HbSsensitivity/diagnostics/nameHbSbestmodel.csv"
     )
-    }
-}
 
-message("++ Great success! Diagnostic and figure 1 (top panels) plots completed." )
+message("++ Great success! Diagnostic and figure 1 (left panels) plots completed." )
 #save(xyt,A,spde,iset,extpoly,mymesh,file=paste0("output/HbS_Fig1.Rdata"))
 message("End HbS_model_diagnosis.R")
 #END
