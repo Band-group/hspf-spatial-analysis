424cd11f02a6de072754eb4bb871071f4ad8e676
Peter Todd <ptr.tdd@gmail.com>
2025-07-09 15:11:48 +0100

    stronger type for colours, used as basis for Country

--- theory/html/hspf-gpu/src/ComparisonDisplay.ts
+++ theory/html/hspf-gpu/src/ComparisonDisplay.ts
@@ -3,9 +3,63 @@ import { PfsaCounts, PixelCoords, LatLong } from './Types.js' ;
 import Geom from './Geom.js' ;
 import GridData from './GridData.js' ;
 
+
+const colours = {
+	"Morocco": "#292933",
+	"Mauritania": "#090953",
+	"Gambia": "#0c0c83",
+	'Senegal': "#2323f6",
+	'Guinea-Bissau': "#0000CD",
+	'Guinea': "#3a3a9f",
+	"Mali": "#42426F",
+	"Sierra Leone": "#42628F",
+	"Liberia": "#42628F",
+	"Burkina_Faso": "#377EB8",
+	"Burkina Faso": "#377EB8",
+	"IvoryCoast": "#2ecdab",
+	"Ivory Coast": "#2ecdab",
+	"Cote_dIvoire": "#2ecdab",
+	"Cote d'Ivoire": "#2ecdab",
+	"Ghana": "#03B4CC",
+	"Benin": "#03cc53",
+	"Nigeria": "#a57d0f",
+	"Niger": "#c57d0f",
+	"Chad": "#fecb00",
+	"Cameroon": "#007a5e",
+	"Gabon": "#009E60",
+	"Republic of the Congo": "#dc241f",
+	"Democratic_Republic_of_the_Congo": "#ef3340",
+	"Democratic Republic of the Congo": "#ef3340",
+	"Congo": "#ef3340",
+	"Rwanda": "#e5be01",
+	"Zambia": "#A4081C",
+	"Sudan": "#c59d0f",
+	"Uganda": "#fcdc04",
+	"Malawi": "#A65628",
+	"Tanzania": "#EE5C42",
+	"United Republic of Tanzania": "#EE5C42",
+	"Mozambique": "#EE5C42",
+	"Kenya": "#FF7F00",
+	"Ethiopia": "#d1cd0c",
+	"Madagascar": "#c800ff",
+	'Bangladesh': "#444444",
+	'Myanmar': "#444444",
+	'Laos': "#444444",
+	'Thailand': "#444444",
+	'Cambodia': "#444444",
+	'Vietnam': "#444444",
+	'Indonesia': "#444444",
+	'PNG': "#444444",
+	'South Africa': "#23f623",
+	'eSwatini': "#23f623",
+	"other": "#AAAAAA"
+} as const;
+
+type Country = keyof typeof colours;
+
 interface PlotPt {
 	index: number,
-	country: string,
+	country: Country,
 	admin1: string,
 	latlong: LatLong,
 	xy: PixelCoords,
@@ -14,18 +68,19 @@ interface PlotPt {
 	N: number
 } ;
 
+
 interface Scales {
 	x: d3.ScaleLinear<number, number>,
 	y: d3.ScaleLinear<number, number>,
-	fill: (d: string) => string
+	fill: (d: Country) => string
 } ;
+
 
 export default class ComparisonDisplay {
 	counts: Array<PfsaCounts> ;
 	scales: Scales ;
 	geom: Geom ;
 	elt: SVGElement ;
-	colours: { [key: string]: string } ;
 	names: { [key: string]: string } ;
 
 	constructor(
@@ -55,57 +110,6 @@ export default class ComparisonDisplay {
 			y: y_column_name,
 			N: N_column_name
 		} ;
-		this.colours = {
-			"Morocco": "#292933",
-			"Mauritania": "#090953",
-			"Gambia": "#0c0c83",
-			'Senegal': "#2323f6",
-			'Guinea-Bissau': "#0000CD",
-			'Guinea': "#3a3a9f",
-			"Mali": "#42426F",
-			"Sierra Leone": "#42628F",
-			"Liberia": "#42628F",
-			"Burkina_Faso": "#377EB8",
-			"Burkina Faso": "#377EB8",
-			"IvoryCoast": "#2ecdab",
-			"Ivory Coast": "#2ecdab",
-			"Cote_dIvoire": "#2ecdab",
-			"Cote d'Ivoire": "#2ecdab",
-			"Ghana": "#03B4CC",
-			"Benin": "#03cc53",
-			"Nigeria": "#a57d0f",
-			"Niger": "#c57d0f",
-			"Chad": "#fecb00",
-			"Cameroon": "#007a5e",
-			"Gabon": "#009E60",
-			"Republic of the Congo": "#dc241f",
-			"Democratic_Republic_of_the_Congo": "#ef3340",
-			"Democratic Republic of the Congo": "#ef3340",
-			"Congo": "#ef3340",
-			"Rwanda": "#e5be01",
-			"Zambia": "#A4081C",
-			"Sudan": "#c59d0f",
-			"Uganda": "#fcdc04",
-			"Malawi": "#A65628",
-			"Tanzania": "#EE5C42",
-			"United Republic of Tanzania": "#EE5C42",
-			"Mozambique": "#EE5C42",
-			"Kenya": "#FF7F00",
-			"Ethiopia": "#d1cd0c",
-			"Madagascar": "#c800ff",
-			'Bangladesh': "#444444",
-			'Myanmar': "#444444",
-			'Laos': "#444444",
-			'Thailand': "#444444",
-			'Cambodia': "#444444",
-			'Vietnam': "#444444",
-			'Indonesia': "#444444",
-			'PNG': "#444444",
-			'South Africa': "#23f623",
-			'eSwatini': "#23f623",
-			"other": "#AAAAAA"
-		} ;
-		let self = this ;
 		this.scales = {
 			x: d3.scaleLinear()
 				.domain( [0,limit] )
@@ -113,7 +117,7 @@ export default class ComparisonDisplay {
 			y: d3.scaleLinear()
 				.domain( [0,limit] )
 				.range( [ this.geom.height - this.geom.margins.bottom, this.geom.margins.top ]),
-			fill: (country: string) => self.colours[country] || self.colours['other']
+			fill: (country: Country) => colours[country] || colours['other']
 			//fill: new d3.scaleThreshold(
 			//	[ -5, 3, 11, 19, 27 ],
 			//	[ '#0500ce', '#06b4cd', '#30504e', '#34cc33', '#2f3dc1', '#db624d' ]
